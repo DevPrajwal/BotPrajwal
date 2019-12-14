b79b7e2e259dae53a5ac6448500eaa1895103e0f
DevPrajwal <forsellid2018@gmail.com>
2019-12-14 16:28:49 +0000

    Much

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-	res.send('hi,I am a bot');
+	res.send("hi,I am a bot");
 });
 
 app.get('/webhook/', function(req, res) {
-	if (req.query['hub.verify_token'] == 'devPrajwal') {
+	if (req.query['hub.verify_token'] == "devPrajwal") {
 		res.send(res.query['hub.challenge']);
 	}
-	res.send('wrong token');
+	res.send("wrong token" );
 }); 
 
 app.listen(app.get('port'),function(){ 
