d143cfaf4b186a5606671cd2c27a58fc4c24ba47
DevPrajwal <forsellid2018@gmail.com>
2019-12-14 05:29:10 +0000

    Null

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const request = require('request');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.port || 5000);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+	res.send('hi,I am a bot');
+});
+
+app.get('/webhook/', function(req, res) {
+	if (req.query['hub.verify_token'] == 'devPrajwal') {
+		res.send(res.query['hub.challenge']);
+	}
+	res.send('wrong token');
+});
